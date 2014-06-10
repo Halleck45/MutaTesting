554a1d5b9ad181be56f7f1146f972758ddfc96d1
Halleck45 <jeanfrancois@lepine.pro>
2014-06-10 13:43:50 +0200

    New mathematical mutaters

---
+++ src/Hal/MutaTesting/Mutater/MutaterMathLess.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hal\MutaTesting\Mutater;
+
+use Hal\Component\Token\Token;
+use Hal\MutaTesting\Mutation\MutationInterface;
+
+class MutaterMathLess extends MutaterSimpleAbstract implements MutaterInterface
+{
+
+    public function mutate(MutationInterface $original, $index)
+    {
+        return $this->mutateOne($original, $index, T_STRING, new Token(array(T_STRING, '>')));
+    }
+
+}
+
